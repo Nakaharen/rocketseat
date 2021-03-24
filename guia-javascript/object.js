f4cbb953ae3fe790f435760920e64f9f7cb8adc8
Karen Nakahara <karenakahara@gmail.com>
2021-03-24 11:35:21 -0300

    objetos

---
+++ guia-javascript/object.js
@@ -0,0 +1,16 @@
+/*
+* Object
+  * Objeto
+  * Propriedades/ Atributos
+  * Funcionalidades / Métodos
+
+{ propriedade : "valor"}
+
+*/
+console.log({
+  name: "Karen",
+  age: 27,
+  walk: function() {
+    console.log('walk')
+  }
+})
