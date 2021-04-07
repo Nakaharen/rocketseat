9bfb88f8cd4cf07c9e197edd25492160f99b9640
Karen Nakahara <karenakahara@gmail.com>
2021-04-06 23:13:36 -0300

    revisao array

---
+++ guia-javascript/praticas/index.js
@@ -0,0 +1,13 @@
+// Array
+
+const animals = [
+  'Rabbit',
+  'Dog',
+  'Cat'
+]
+
+// acessar valores dentro do array
+// a lista começa no 0
+// uma forma de organizar os dados
+
+console.log(animals[0])
