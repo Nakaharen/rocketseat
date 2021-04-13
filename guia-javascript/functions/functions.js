12f5e6656c52405718ca6e6869d19f38fc1ac428
Karen Nakahara <karenakahara@gmail.com>
2021-04-12 22:20:07 -0300

    functions

---
+++ guia-javascript/functions/functions.js
@@ -0,0 +1,19 @@
+// criar um app de frases motivacionais
+// declaration - declaração da função
+// function statement
+// agrupamento de código e reutilização desse código
+
+function createPhrases() {
+console.log('Não pare até se orgulhar de você')
+console.log('Imagine uma nova história para sua vida e acredite nela')
+console.log('A verdadeira motivação não é aquilo que te anima, mas aquilo que te transforma')
+}
+/* executar a função
+  rodar, chamar, invocar
+  execute, run, call, invoke */
+
+  createPhrases()
+  createPhrases()
+  createPhrases()
+
+  console.log('fim do programa')
