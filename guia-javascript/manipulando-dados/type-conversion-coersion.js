960a32a2ba62e9c9c5084141bdd173f1e2bc7c30
Karen Nakahara <karenakahara@gmail.com>
2021-04-21 23:29:38 -0300

    type conversion coersion

---
+++ guia-javascript/manipulando-dados/type-conversion-coersion.js
@@ -0,0 +1,7 @@
+/*
+    Type conversion (typecasting) vs Type coersion
+    EU altero um dado de um tipo para outro  / O JS implicitamente fazendo essa troca
+    * Alteração de um tipo de dado para outro tipo
+
+*/
+
