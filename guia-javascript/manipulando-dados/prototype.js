eb714afd767c685947ef02763521684585374d34
Karen Nakahara <karenakahara@gmail.com>
2021-04-21 23:18:59 -0300

    prototype

---
+++ guia-javascript/manipulando-dados/prototype.js
@@ -0,0 +1,11 @@
+/*
+
+    Prototype
+
+    * prototype-base language
+    * prototype chain
+    * _proto_
+
+  prototype é capaz de armazenar propriedades e métodos de um objeto, arrays, etc
+
+*/
