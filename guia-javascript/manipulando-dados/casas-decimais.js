5ad83d698dff0aecc47ec72fac40e6daebd4c93e
Karen Nakahara <karenakahara@gmail.com>
2021-04-22 21:46:59 -0300

    casas decimais

---
+++ guia-javascript/manipulando-dados/casas-decimais.js
@@ -0,0 +1,4 @@
+// Transformar um número quebrado com 2 casas decimais e trocar ponto por vírgula
+let number = 374.1493679
+console.log(number.toFixed(2).replace(".",","))
+// o retorno é uma string e por ser uma string é possível atrelar mais métodos
