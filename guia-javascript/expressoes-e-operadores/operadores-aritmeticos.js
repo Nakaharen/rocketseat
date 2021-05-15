760cad667d46baed7914da40ff9055f1c3651a2d
Karen Nakahara <karenakahara@gmail.com>
2021-05-15 17:49:58 -0300

    operadores aritmeticos

---
+++ guia-javascript/expressoes-e-operadores/operadores-aritmeticos.js
@@ -0,0 +1,32 @@
+// Operadores Aritméticos
+
+// Multiplicação *
+// console.log(3.2 * 5)
+
+// Divisão /
+// console.log(25 / 2)
+
+// Soma +
+// console.log(35+59)
+
+// Subtração -
+// console.log(27-13)
+
+// Resto da divisão %
+let remainder
+remainder= 11 % 3
+// console.log(remainder)
+
+// Incremento ++
+let increment = 0
+increment++
+// console.log(increment)
+
+// Decremento
+let decrement = 0
+decrement--
+// console.log(decrement)
+
+// Exponencial **
+// console.log(3 ** 3)
+
