e598d83115eed630d17a39db6358adb2ccb769b9
Karen Nakahara <karenakahara@gmail.com>
2021-05-15 18:22:14 -0300

    estritamente igual e estritamente diferente

---
+++ guia-javascript/expressoes-e-operadores/estritamente-igual-diferente.js
@@ -0,0 +1,18 @@
+// Operadores de comparação
+// Estritamente igual a/ Estritamente diferente de
+
+let one = 1
+let two = 2
+
+// ===  estritamente igual a
+// console.log(one === "1")
+// != por ser string
+// console.log(one === 1)
+// = por ser number
+
+// !==  estritamente diferente de
+console.log( two !== "2")
+// true por ser diferente no tipo
+console.log( two !== 2)
+// false por ser igual no valor e no tipo
+
