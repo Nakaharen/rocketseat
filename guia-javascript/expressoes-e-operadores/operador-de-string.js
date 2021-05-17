198236784cccd476f21dda90461e537a31611b74
Karen Nakahara <karenakahara@gmail.com>
2021-05-16 22:43:41 -0300

    operador de string

---
+++ guia-javascript/expressoes-e-operadores/operador-de-string.js
@@ -0,0 +1,9 @@
+// Operador de String (String operator)
+
+// comparison
+//console.log( 'a' == 'b')
+
+// concatenation
+// Retorna a união de duas Strings
+let alpha = 'alpha'
+console.log(alpha + 'bet')
