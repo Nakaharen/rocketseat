a89bc9824601c21dec491970c9fe4367cec49836
Karen Nakahara <karenakahara@gmail.com>
2021-05-17 21:36:15 -0300

    for of

---
+++ guia-javascript/estrutura-de-repeticao/for-of.js
@@ -0,0 +1,13 @@
+//for...of
+
+let name = 'Karen'
+let names = ['Guilherme, Camila, Luiza']
+
+/*for(let char of name) {
+  console.log(char)
+}
+*/
+
+for(let name of names) {
+    console.log(name)
+}
